35ff5dec60dfb2a4a2849637485892faa54222b1
Gabriel Parke <gabrielparke@Gabriels-MBP-2.fios-router.home>
2018-03-18 12:48:26 -0400

    Setup apollo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  cache: new InMemoryCache()
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
