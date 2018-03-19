d0ec97c155c717be51ada2f8e16990935ff9250b
Gabriel Parke <gabrielparke@Gabriels-MBP-2.fios-router.home>
2018-03-18 22:51:35 -0400

    Add login/signup icon assets

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 import Login from '../mutations/Login';
 import CurrentUser from '../queries/CurrentUser';
 
+import facebookIcon from '../assets/images/icons/facebook_white.svg';
+import googleIcon from '../assets/images/icons/google_white.svg';
+
 class LoginForm extends Component {
   constructor(props) {
     super(props);
@@ -18,8 +21,8 @@ class LoginForm extends Component {
     // nextProps is the props that will be placed on our component the next time it re-renders
     // this.props is the old props and nextProps are the new props
     if (!this.props.data.user && nextProps.data.user) {
-       // redirect to about for now
-       // NOTE: not sure if this is the right way to do it
+      // redirect to about for now
+      // NOTE: not sure if this is the right way to do it
       this.props.history.push('/about');
     }
   }
@@ -35,11 +38,14 @@ class LoginForm extends Component {
     });
   }
 
+  // NOTE: very similar to signup form, look into refactoring these 2 components
   render() {
     return (
       <div>
         <h5>Login</h5>
         <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
+        <img src={facebookIcon} className="login-icon" alt="facebook login" />
+        <img src={googleIcon} className="login-icon" alt="google login" />
         <Link to="/signup">Sign Up</Link>
       </div>
     );
