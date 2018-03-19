ac6cb8fe591742bc027abb20d02eef607ab27732
Gabriel Parke <gabrielparke@Gabriels-MBP-2.fios-router.home>
2018-03-18 22:10:11 -0400

    Add in react router scaffolding, Update apollo client to allow credentials for authentication

--- src/index.js
+++ src/index.js
@@ -5,19 +5,35 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// NOTE: if i pass id back from requests, dataIdFromObject will update data without having to re-request it
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
-  cache: new InMemoryCache()
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql', credentials: 'include' }),
+  cache: new InMemoryCache(),
+  dataIdFromObject: o => o.id
 });
+
+// test component for routing
+const About = () => {
+  return <h1>About</h1>;
+};
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <App>
+              <Route path="/about" component={About} />
+            </App>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 };
