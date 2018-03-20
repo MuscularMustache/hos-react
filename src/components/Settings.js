14dea2aa83de0193719a27118b2a430562f11383
Gabriel Parke <gabrielparke@gmail.com>
2018-03-20 17:13:52 -0400

    Add logout functionality to settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { graphql } from 'react-apollo';
+import CurrentUser from '../queries/CurrentUser';
+import Logout from '../mutations/Logout';
 
 class Settings extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query: CurrentUser }]
+    });
+  }
+
   render() {
     return (
       <div className="content">
         <h2>Settings</h2>
         <Link className="standard-btn" to="/">Back</Link>
+        <a className="standard-btn" onClick={this.onLogoutClick.bind(this)}>logout</a>
       </div>
     );
   }
 }
 
-export default Settings;
+export default graphql(Logout)(Settings);
