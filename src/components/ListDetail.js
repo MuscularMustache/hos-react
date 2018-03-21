8d15d39decd456b632d35b555e96fb4fb6402e6a
Gabriel Parke <gabrielparke@gmail.com>
2018-03-21 01:09:17 -0400

    Add ability to delete lists

--- src/components/ListDetail.js
+++ src/components/ListDetail.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import FetchList from '../queries/FetchList';
+import FetchLists from '../queries/FetchLists';
+import DeleteList from '../mutations/DeleteList';
 import { Link } from 'react-router-dom';
 import ConsequenceList from './ConsequenceList';
 import LoadingIndicator from './LoadingIndicator';
 
 class ListDetail extends Component {
+  onListDelete(id) {
+    this.props.mutate({
+        variables: { id },
+        refetchQueries: [{ query: FetchLists }]
+      })
+      .then(() => {
+        // NOTE: not sure if this is the right way to do it
+        this.props.history.push('/lists');
+      });
+  }
+
   render() {
     const { list } = this.props.data;
 
@@ -14,6 +27,7 @@ class ListDetail extends Component {
     return (
       <div className="content">
         <h2>{list.title}</h2>
+        <a onClick={() => this.onListDelete(list.id)}>delete list</a>
         <ConsequenceList consequences={list.consequences} />
         <Link className="standard-btn" to="/lists">Back</Link>
       </div>
@@ -21,6 +35,6 @@ class ListDetail extends Component {
   }
 }
 
-export default graphql(FetchList, {
+export default graphql(DeleteList)(graphql(FetchList, {
   options: (props) => { return { variables: { id: props.match.params.id } } }
-})(ListDetail);
+})(ListDetail));
