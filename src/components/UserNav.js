750c5f6acd0a6d2b332927dc54d7634295fdc706
Gabriel Parke <gabrielparke@gmail.com>
2018-05-15 15:36:46 -0400

    Style main menu landing area

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoadingIndicator from './LoadingIndicator';
+import '../styles/user-nav.css'
 
 const UserNav = props => {
   if (props.data.loading) { return <LoadingIndicator />; }
 
   return (
-    <div className="content">
+    <div className="content main-menu">
       <Link className="standard-btn" to="/game">play</Link>
       <Link className="standard-btn" to="/lists">edit consequences</Link>
       <Link className="standard-btn" to="/settings">settings</Link>
