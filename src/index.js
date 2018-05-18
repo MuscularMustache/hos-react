4c8bbfffabdc80614635b46d0e7a1ee26b037587
Gabriel Parke <gabrielparke@gmail.com>
2018-05-18 10:41:51 -0400

    fix index listing issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './styles/index.css';
 
 import App from './components/App';
 import Login from './components/login/LoginForm';
@@ -18,34 +18,34 @@ import StartGame from './components/game/StartGame';
 import requireAuth from './components/requireAuth';
 import registerServiceWorker from './registerServiceWorker';
 
-// NOTE: if i pass id back from requests, dataIdFromObject will update data without having to re-request it
+// NOTE: if i pass id back from requests
+// - dataIdFromObject will update data without having to re-request it
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:4000/graphql', credentials: 'include' }),
   cache: new InMemoryCache(),
   dataIdFromObject: o => o.id
 });
 
-const Root = () => {
-  return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <App>
-              <Route path="/" exact component={requireAuth(UserNav)} />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/settings" component={requireAuth(Settings)} />
-              <Route path="/lists" exact component={requireAuth(ListGroup)} />
-              <Route path="/lists/:id" component={requireAuth(ListDetail)} />
-              <Route path="/game" component={requireAuth(StartGame)} />
-            </App>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </ApolloProvider>
-  );
-};
+const Root = () => (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <App>
+            <Route path="/" exact component={requireAuth(UserNav)} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/settings" component={requireAuth(Settings)} />
+            <Route path="/lists" exact component={requireAuth(ListGroup)} />
+            <Route path="/lists/:id" component={requireAuth(ListDetail)} />
+            <Route path="/game" component={requireAuth(StartGame)} />
+          </App>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </ApolloProvider>
+);
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
