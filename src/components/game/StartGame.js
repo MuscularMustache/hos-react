01373d239dd0e3a665fdbf702a980d4d04a9ae4f
Gabriel Parke <gabrielparke@gmail.com>
2018-05-19 14:08:12 -0400

    Fix game state - stale data issue

--- src/components/game/StartGame.js
+++ src/components/game/StartGame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import ResetGame from './ResetGame';
@@ -9,6 +10,24 @@ import FetchGame from '../../queries/FetchGame';
 import '../../styles/game.css';
 
 class StartGame extends Component {
+  // TODO: REFACTOR THIS - still looks like crap
+  componentDidUpdate() {
+    const { game, loading } = this.props.data;
+    // eslint-disable-next-line no-undef
+    const storedConsequences = JSON.parse(localStorage.getItem('activeGame'));
+
+    if (!game || loading || _.get(storedConsequences, '', []).length !== 0) { return; }
+
+    if (game.length === 0) { this.startGame(this.props.userId); }
+
+    const consequences = _.get(game, '[0].lists', []).reduce((arr, list) => [...arr, ...list.consequences], []);
+
+    if (consequences === undefined) { return; }
+
+    // eslint-disable-next-line no-undef
+    localStorage.setItem('activeGame', JSON.stringify(consequences));
+  }
+
   startGame(userId) {
     this.props.mutate({
       variables: { userId },
@@ -26,39 +45,13 @@ class StartGame extends Component {
     // eslint-disable-next-line no-undef
     localStorage.removeItem('activeGame');
     this.props.history.push('/');
-    // this.props.data.refetch();
-  }
-
-  // TODO: REFACTOR THIS - looks like crap - should this be its own component?
-  initializeGame() {
-    const { game, loading } = this.props.data;
-    // eslint-disable-next-line no-undef
-    const storedConsequences = JSON.parse(localStorage.getItem('activeGame'));
-
-    if (!game || loading) { return; }
-
-    if (game.length === 0) { this.startGame(this.props.userId); }
-
-    // NOTE: the only reason this isn't consolodated with the !game || loading
-    // - is because if theres no game but there are stored consequences reset game doesn't appear
-    // - (only happens if i manually delete the game out of the database)
-    if (storedConsequences && storedConsequences.length !== 0) { return; }
-
-    const consequences = game[0] && game[0].lists.reduce((arr, list) => {
-      return [...arr, ...list.consequences];
-    }, []);
-
-    if (consequences === undefined) { return; }
-
-    // eslint-disable-next-line no-undef
-    localStorage.setItem('activeGame', JSON.stringify(consequences));
+    this.props.data.refetch();
   }
 
   // NOTE: menu items need to be .standard-btn and have inner spans along with an icon class
   render() {
     return (
       <div className="content">
-        {this.initializeGame()}
         <ConsequenceChoice />
 
         <Menu>
