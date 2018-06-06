2f671b32ca974b6318a6dbe54a217d3d4a5d3eb3
Gabriel Parke <gabrielparke@gmail.com>
2018-06-06 00:53:39 -0400

    Create custom wipeout for editing and deleting consequences

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     'arrow-parens': [ 'error', 'as-needed' ],
     'comma-dangle': [ 'error', 'never' ],
+    'max-len': 0,
     'react/jsx-filename-extension': [ 1, { 'extensions': ['.js', '.jsx'] } ],
     'react/prop-types': [ 0 ],
     'jsx-a11y/no-static-element-interactions': 0,
