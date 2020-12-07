e4d35dac1b7afc4e8ad462fd9414368ba2d2fc4c
Gabriel Parke <gabrielparke@gmail.com>
2020-12-07 13:33:34 -0500

    Patch vulnerabilities and update dependencies

--- src/components/lists/ListDetail.js
+++ src/components/lists/ListDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { graphql, compose } from 'react-apollo';
+import { graphql } from 'react-apollo';
+import {flowRight as compose} from 'lodash';
 import { Link } from 'react-router-dom';
 import FetchList from '../../queries/FetchList';
 import FetchLists from '../../queries/FetchLists';
